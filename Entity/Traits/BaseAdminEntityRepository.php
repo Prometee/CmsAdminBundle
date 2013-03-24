5d97bcbf1445c7e189ee20767628a40f1f2ac70b
Francis Hilaire <prometee@gmail.com>
2013-03-24 20:26:14 +0100

    Put BaseAdminEntityRepository to a trait

---
+++ Entity/Traits/BaseAdminEntityRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Cms\Bundle\AdminBundle\Entity\Traits;
+
+trait BaseAdminEntityRepository {
+	
+	public function deleteGroup($ids) {
+        $qb = $this->createQueryBuilder('t');
+
+        $qb->delete()
+                ->where($qb->expr()->in('t.id', $ids))
+                ->getQuery()
+                ->execute();
+
+        $this->_em->flush();
+    }
+}
