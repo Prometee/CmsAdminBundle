064b7dc11d0654b49e86448cef631b9da23f5649
Francis Hilaire <prometee@gmail.com>
2013-05-17 17:25:00 +0200

    Add DataTransformer to use in widget

---
+++ Form/DataTransformer/AbstractEntityToIntegerTransformer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Cms\Bundle\AdminBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class AbstractEntityToIntegerTransformer implements DataTransformerInterface {
+	
+	protected $om;
+    protected $repository = 'CmsAdminBundle:Foo';
+	
+	public function __construct(ObjectManager $om) {
+        $this->om = $om;
+    }
+	
+	public function transform($entity) {
+		
+		$id = null;
+		
+		if (null !== $entity) {
+            $id = $entity->getId();
+        }
+		
+		return (int) $id;
+	}
+	
+	public function reverseTransform($id) {
+		
+		if (!$id) {
+            return null;
+        }
+
+        $entity = $this->om
+            ->getRepository($this->repository)
+            ->findOneById($id)
+        ;
+        
+        if ($entity === null) {
+            throw new TransformationFailedException();
+        }
+
+        return $entity;
+	}
+}
+
+?>
