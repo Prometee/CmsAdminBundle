7e11f7b5f7cf1db72f53126873b1967c1f0be039
Francis Hilaire <francis@force-interactive.com>
2016-10-03 17:47:45 +0200

    NameSpace change from Entity to Model directory for entity User

--- Entity/User.php
+++ Entity/User.php
@@ -2,7 +2,7 @@
 
 namespace Cms\Bundle\AdminBundle\Entity;
 
-use Cms\Bundle\AdminBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
